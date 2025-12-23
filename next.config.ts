b32184561df9caad301e28547c4f5e11179e68b8
Serkan Aysan <serkanaysann@gmail.com>
2025-12-23 15:20:11 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   i18n: {
     locales: ["en", "tr"],
     defaultLocale: "en",
+    localeDetection: false, // otomatik /tr veya /en yönlendirmesini kapatır
   },
   images: {
     remotePatterns: [
