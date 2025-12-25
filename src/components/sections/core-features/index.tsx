52e8c3b443e28d9cf71c1977529c821a64e636d1
Serkan Aysan <serkanaysann@gmail.com>
2025-12-25 11:41:44 +0300

    Update index.tsx

--- src/components/sections/core-features/index.tsx
+++ src/components/sections/core-features/index.tsx
@@ -5,7 +5,7 @@ export function CoreFeatures() {
   return (
     <section className="py-30 bg-gray-100 dark:bg-white/1 px-5">
       <div className="max-w-[72rem] mx-auto">
-        <div className="mb-12 text-center">
+        {/* <div className="mb-12 text-center">
           <h2 className="mb-3 font-bold text-gray-800 text-3xl dark:text-white/90 md:text-title-lg max-w-xl mx-auto">
             Core Features of our Tools
           </h2>
@@ -15,7 +15,7 @@ export function CoreFeatures() {
             Transforming Your Ideas into Reality with Unmatched Precision and
             Intelligence.
           </p>
-        </div>
+        </div> */}
 
           <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-6 sm:gap-8">
           {CORE_FEATURES.map((feature) => (
